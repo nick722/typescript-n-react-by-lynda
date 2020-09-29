6aedf949f3e5dec92e123c05c9038cf1130bf018
nick722 <nick22783@gmail.com>
2020-09-29 15:38:25 +0300

    3.2 Define our state

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import logo from "./logo.svg";
 import Message from "./Message";
 import "./App.css";
 
-class App extends Component<any> {
+const initialState = {
+  name: "Piper",
+  message: "TypeScript is cool!!"
+};
+
+type State = Readonly<typeof initialState>;
+
+class App extends Component<any, State> {
+  readonly state: State = initialState;
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Message name="Piper" message="this a typed message" />
+          <Message name={this.state.name} message={this.state.message} />
         </header>
       </div>
     );
