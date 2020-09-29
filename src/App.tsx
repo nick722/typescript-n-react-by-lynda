e3bf38cea7887508082403ba23d1c60b433de5b2
nick722 <nick22783@gmail.com>
2020-09-29 15:10:30 +0300

    2.4 Functional/stateless component intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Message from "./Message";
+import "./App.css";
 
 class App extends Component {
   render() {
-    // tuple
-    let aTuple: [string, number] = ['Piper', 37];
-    // enum
-    enum Codes {fist = 1, second = 'Curie'}
-    // any
-    let firstName: any =  37
-    // void
-    const warning = (): void => {
-      console.log('Warning');
-    }
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            The value {firstName} is of {typeof firstName} type!
-          </p>
+          <Message />
         </header>
       </div>
     );
