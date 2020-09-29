605faebd8f058c74b8115ff88923eb84f3809f97
nick722 <nick22783@gmail.com>
2020-09-29 15:31:19 +0300

    3.1 Introduction to interface

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,12 @@ import Message from "./Message";
 import "./App.css";
 
 class App extends Component<any> {
-  componentWillMount() {
-    console.log("Almost there...");
-  }
-
-  componentDidMount() {
-    console.log("Finally...hello!");
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Message message="This a typed message" />
+          <Message name="Piper" message="this a typed message" />
         </header>
       </div>
     );
